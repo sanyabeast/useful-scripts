7e63b7e1b771419b6522243dead1ae4f1dd81cb6
sanyabeast <a.gvrnsk@gmail.com>
2023-05-01 19:42:17 +0300

    chatworks/run_in_termnal

--- chatworks/run_in_terminal.sh
+++ chatworks/run_in_terminal.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-# Check if the script received exactly two arguments
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <terminal-emulator> \"<command>\""
+# Check if the script received two or three arguments
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <terminal-emulator> \"<command>\" [\"<title>\"]"
     exit 1
 fi
 
 # Use the arguments in the command
-$1 -e "bash -c '$2; read -n1 -r -p \"Press any key to close the terminal...\"'"
+if [ "$#" -eq 3 ]; then
+    title="$3"
+else
+    title="terminal"
+fi
+
+# Get current time and date
+launch_time=$(date "+%Y-%m-%d %H:%M:%S")
+
+# Combine launch_time and title
+terminal_title="$launch_time - $title"
+
+$1 -T "$terminal_title" -e "bash -c '$2; read -n1 -r -p \"Press any key to close the terminal...\"'"
